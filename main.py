36397415b1f14ba0526ecc7e3e3da252d0cf9d8d
Sven Schoenherr <s.schoenherr93@gmail.com>
2022-02-12 13:45:02 +0100

    Add counter to finish game latest after 9 moves

--- main.py
+++ main.py
@@ -25,17 +25,20 @@ print(logo)
 print("Welcome to Tic Tac Toe! This is a two player game to place your symbol 'X' or 'O' into the grid. The one having three in one row wins! \nPlease choose your position by typing a number between 1 and 9.\n")
 print(board)
 
+count = 0
+
 # Ask for players move as long as game has not finished
 game_is_finished = False
 while not game_is_finished:
     move = input("Please choose between 1 and 9 to set your mark: ")
+    count += 1
 
     # Add players moves to board
     board = board.replace(move, "O")
     print(board)
 
-    game_is_finished = True
-# TODO Add players moves to board
+    if count == 9:
+        game_is_finished = True
 
 # TODO Implement game logic to end game
 
