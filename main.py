14dd884dcf5e3e4a3a097f82ebd2023d4ae7875a
Sven Schoenherr <s.schoenherr93@gmail.com>
2022-02-12 13:30:42 +0100

    Add development outline

---
+++ main.py
@@ -0,0 +1,10 @@
+# TODO Implement board
+# TODO Implement instructions for player
+# TODO Ask for players move as long as game has not finished
+# TODO Add players moves to board
+# TODO Implement game logic to end game
+# TODO Feat1
+    # Implement input checks for not allowed chars,
+    # already blocked positions
+# TODO Feat2
+    # Implement random "KI" moves
